7b571da5f499919a021fa0374534951a26b865a3
pengbo@kuaicto.com <pengbo@kuaicto.com>
2018-11-22 20:32:50 +0800

    install vue

--- show-project/src/main.ts
+++ show-project/src/main.ts
@@ -2,12 +2,8 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
-// @ts-ignore
-/*import Antd from 'ant-design-vue';*/
-/*import 'ant-design-vue/dist/antd.css';*/
 import './style/app.less';
 Vue.config.productionTip = false;
-/*Vue.use(Antd);*/
 new Vue({
   router,
   store,
